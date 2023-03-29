8da6a30e9cca55b3808441f41b9ae7f1c05e0f1f
ajagyabeng <ajagyabeng@gmail.com>
2023-03-29 18:39:41 +0000

    Add routers to urls

--- piggybank/piggybank/urls.py
+++ piggybank/piggybank/urls.py
@@ -1,15 +1,18 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from rest_framework import routers
 
 from core import views
 
 router = routers.SimpleRouter()
 router.register(r'categories', views.CategoryModelViewSet, basename='category')
+router.register(r'transactions', views.TransactionModelViewSet,
+                basename='transaction')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('currencies/', views.CurrencyListAPIView.as_view(), name='currencies')
+    path('currencies/', views.CurrencyListAPIView.as_view(), name='currencies'),
+    path('', include(router.urls))
 ]
 
-urlpatterns += router.urls
+# urlpatterns += router.urls
