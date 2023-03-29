3a2ce6588495e9f8980ebbd0a2366b65f2d5708a
ajagyabeng <ajagyabeng@gmail.com>
2023-03-29 18:39:02 +0000

    Create views

--- piggybank/core/views.py
+++ piggybank/core/views.py
@@ -1,8 +1,11 @@
 from rest_framework.generics import ListAPIView
 from rest_framework.viewsets import ModelViewSet
 
-from .models import Currency, Category
-from .serializers import CurrencySerializer, CategorySerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
+from .models import Currency, Category, Transaction
+from .serializers import CurrencySerializer, CategorySerializer, TransactionSerializer, ReadTransactionSerializer, WriteTransactionSerializer
 
 
 class CurrencyListAPIView(ListAPIView):
@@ -16,3 +19,23 @@ class CurrencyListAPIView(ListAPIView):
 class CategoryModelViewSet(ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = None
+
+
+class TransactionModelViewSet(ModelViewSet):
+    """When using a foreign key, tell django to pre-fetch them(select_related) prior to a query. It makes it faster"""
+    # queryset = Transaction.objects.all()
+    queryset = Transaction.objects.select_related("currency", "category")
+
+    # FILTER
+    filter_backends = [filters.SearchFilter,
+                       filters.OrderingFilter, DjangoFilterBackend]
+    search_fields = ["description"]
+    ordering_fields = ["amount"]
+    filterset_fields = ["currency__code"]
+
+    def get_serializer_class(self):
+        """ModelViewSet adds all the actions(List, Create, Retrieve, Update, Destroy) on the view"""
+        if self.action in ("list", "retrieve"):
+            return ReadTransactionSerializer
+        return WriteTransactionSerializer
