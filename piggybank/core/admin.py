f1c67cda9ef1c9a7023d64bf763f6b6b3e7ceab6
ajagyabeng <ajagyabeng@gmail.com>
2023-04-03 17:25:59 +0000

    Register Transaction and Category models

--- piggybank/core/admin.py
+++ piggybank/core/admin.py
@@ -5,3 +5,5 @@ from django.contrib import admin
 from .models import Currency, Transaction, Category
 
 admin.site.register(Currency)
+admin.site.register(Transaction)
+admin.site.register(Category)
