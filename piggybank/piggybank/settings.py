4f34376ba084b03e8813acd9262c8d9b3daed3d0
ajagyabeng <ajagyabeng@gmail.com>
2023-04-09 18:52:34 +0000

    Add debug_toolbar settings

--- piggybank/piggybank/settings.py
+++ piggybank/piggybank/settings.py
@@ -42,12 +42,13 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'django_filters',
-
+    "debug_toolbar",
 
     'core',
 ]
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
