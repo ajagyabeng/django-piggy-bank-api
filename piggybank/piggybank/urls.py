18a4e429c63763e030673e831ebd6dc3c6b7a307
ajagyabeng <ajagyabeng@gmail.com>
2023-04-09 18:53:23 +0000

    Add debug_toolbar url

--- piggybank/piggybank/urls.py
+++ piggybank/piggybank/urls.py
@@ -13,8 +13,12 @@ router.register(r'transactions', views.TransactionModelViewSet,
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
+
     path('currencies/', views.CurrencyListAPIView.as_view(), name='currencies'),
     path('login/', obtain_auth_token, name="obtain_auth_token"),
     path('report/', views.TransactionReportAPIView.as_view(), name="reports"),
+
+    path('__debug__/', include('debug_toolbar.urls')),
+
     path('', include(router.urls))
 ]
